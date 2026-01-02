e6318449a589ef9fb3aa7e4c9326d2a460aa7865
ervinbani <bani.ervin@gmail.com>
2026-01-02 22:05:54 +0100

    feat: scaffold Vite React+TS app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { Suspense, lazy } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React, { Suspense, lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-const Home = lazy(() => import('./pages/Home'))
-const CountryDetail = lazy(() => import('./pages/CountryDetail'))
+const Home = lazy(() => import("./pages/Home"));
+const CountryDetail = lazy(() => import("./pages/CountryDetail"));
 
 export default function App() {
   return (
@@ -13,5 +13,5 @@ export default function App() {
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Suspense>
-  )
+  );
 }
